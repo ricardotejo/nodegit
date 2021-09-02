5286044c8fa7fa5957d937789afffbaa2e2fbf64
Ricardo Tejo <ricardo.tejo@trilogy.com>
2021-09-02 17:22:06 -0500

    add https

--- arm-bin/server.sh
+++ arm-bin/server.sh
@@ -1,2 +1,4 @@
-http-server ./arm-bin -p 8800 > http.log 2>&1 &
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes -subj '/CN=localhost'
+http-server ./arm-bin -p 8800 --ssl --cert cert.pem --key key.pem > http.log 2>&1 &
 export HTTP_SERVER_PID=$!
+export NODE_TLS_REJECT_UNAUTHORIZED=0
